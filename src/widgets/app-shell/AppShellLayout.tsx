5634202c27e12843fc9902315a4498a1da579316
이지연 <wldus019113@icloud.com>
2026-05-06 03:18:16 +0900

    main 하단 여백 추가 보정

--- src/widgets/app-shell/AppShellLayout.tsx
+++ src/widgets/app-shell/AppShellLayout.tsx
@@ -2046,7 +2046,7 @@ function AppShellLayout() {
             <Layout className="tw-flex tw-min-h-0 tw-min-w-0 tw-flex-1 tw-flex-col tw-bg-slate-50">
                 <AppShellHeader/>
                 <Layout.Content
-                    className="wf-scrollbar tw-flex tw-min-h-0 tw-flex-1 tw-flex-col tw-overflow-y-auto tw-bg-transparent tw-px-6 tw-pt-6 tw-pb-28">
+                    className="wf-scrollbar tw-flex tw-min-h-0 tw-flex-1 tw-flex-col tw-overflow-y-auto tw-bg-transparent tw-px-6 tw-pt-6 tw-pb-40 [scroll-padding-bottom:10rem]">
                     <Outlet/>
                 </Layout.Content>
             </Layout>
